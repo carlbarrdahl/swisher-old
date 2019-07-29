39d6a599205a7b3282d319376ecdd029d33a13f5
Carl Barrdahl <carlbarrdahl@gmail.com>
2019-07-29 12:55:08 +0200

    Fix test

--- __tests__/url.test.js
+++ __tests__/url.test.js
@@ -25,7 +25,7 @@ test("toURLToken", () => {
 })
 test("swishLink", () => {
   expect(swishLink(payment)).toEqual(
-    `swish://payment?data=%7B%22version%22%3A1%2C%22payee%22%3A%7B%22value%22%3A%220723419119%22%7D%2C%22amount%22%3A%7B%22value%22%3A100%7D%2C%22message%22%3A%7B%22value%22%3A%22test%22%2C%22editable%22%3Afalse%7D%7D&callbackurl=http://localhost/done&callbackresultparameter=res`
+    `swish://payment?data={\"version\":1,\"payee\":{\"value\":\"0723419119\"},\"amount\":{\"value\":100},\"message\":{\"value\":\"test\",\"editable\":false}}&callbackurl=http://localhost/done&callbackresultparameter=res`
   )
 })
 
