aab38b929ce3aaabf0adfffae04be9436ac99272
Carl Barrdahl <carlbarrdahl@gmail.com>
2020-01-02 19:24:46 +0100

    add open page

---
+++ src/pages/open.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react"
+import { fromToken, deserialize, decrypt, swishLink } from "../utils"
+
+import Button from "../components/Button"
+import Layout from "../components/Layout"
+import QRCode from "../components/QRCode"
+
+import { withTranslations } from "../providers/Translations"
+
+const PaymentScreen = withTranslations(({ location, t, ...props }) => {
+  const token = location.search.split("?")[1]
+  const payment = fromToken(token)
+  if (!payment.amount) {
+    return null
+  }
+
+  useEffect(() => {
+    window.open(swishLink(payment), "_blank", "location=yes")
+  }, [])
+  return (
+    <Layout title="Payment">
+      <div className="text-lg text-center text-grey-darkest leading-normal">
+        {t("app.payment.opening")}
+      </div>
+    </Layout>
+  )
+})
+
+export default PaymentScreen
